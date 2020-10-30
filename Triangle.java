ab8bdbc657baee04f4978f8ceeff6c9672329808
Justin Dylan <justin82049@gmail.com>
2020-10-29 22:39:53 -0400

    fixed area and classify

--- Triangle.java
+++ Triangle.java
@@ -15,24 +15,38 @@ public Triangle(double x1, double y1,double x2, double y2,double x3, double y3){
     return v1.distanceTo(v2) + v2.distanceTo(v3) + v3.distanceTo(v1);
   }
   public double getArea(){
-    double a = v1.distanceTo(v2);
-    double b = v2.distanceTo(v3);
-    double c = v3.distanceTo(v1);
-    double semiPerimeter = (a + b + c) / 2;
-    double area = Math.sqrt(semiPerimeter * semiPerimeter - a * semiPerimeter - b * semiPerimeter-c);
+    double e = v1.distanceTo(v2);
+    double f = v2.distanceTo(v3);
+    double g = v3.distanceTo(v1);
+    double semiPerimeter = (e + f + g) / 2;
+    double area = Math.sqrt(semiPerimeter * (semiPerimeter - e) * (semiPerimeter - f) * (semiPerimeter - g));
     return area;
   }
   public String classify(){
-    if (v1.distanceTo(v2) == v2.distanceTo(v3) && v2.distanceTo(v3) == v3.distanceTo(v1)){
+    double a = Math.round(v1.distanceTo(v2) *10000.0) / 10000.0;
+    double b = Math.round(v2.distanceTo(v3) *10000.0) / 10000.0;
+    double c = Math.round(v3.distanceTo(v1) *10000.0) / 10000.0;
+    if (a == b && b == c){
       return "equilateral";
     }
-    else if (v1.distanceTo(v2) != v2.distanceTo(v3) && v2.distanceTo(v3) != v3.distanceTo(v1)){
+    if (a != b && b != c && c != a){
       return "scalene";
     }
     return "isosceles";
   }
   public String toString() {
     return ("v1" + v1 + "v2" + v2 + "v3" + v3);
+  }
+  public void setVertex(int index, Point newP){
+    if (index == 0){
+      newP = v1;
+    }
+    if (index == 1){
+      newP = v2;
+    }
+    if (index == 2){
+      newP = v3;
+    }
   }
 
 }
